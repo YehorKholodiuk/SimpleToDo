7fa9dc0d582f10095af95631ae573f018e564ec4
ASUS <Yehor Kholodiuk>
2022-02-04 18:15:48 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {nanoid} from "nanoid";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState([
+        {
+            id: nanoid(),
+            task: 'learn JavaScript'
+        },
+        {
+            id: nanoid(),
+            task: 'learn HTML'
+        },
+        {
+            id: nanoid(),
+            task: 'learn React'
+        }
+    ])
+    const [newTaskInput, setNewTaskInput] = useState('')
+    function addToList(){
+        const newTask = {id: nanoid(), task:newTaskInput}
+        setTasks([...tasks,newTask])
+    }
+    return (
+        <div>
+            <h1>To Do List</h1>
+            <input type="text"
+                   value ={newTaskInput}
+                   onChange={(e) => setNewTaskInput(e.target.value)}
+            />
+            <button onClick={addToList}>Add To List</button>
+            <ul>{tasks.map(e => <li>{e.task}</li>)}</ul>
+        </div>
+    );
 }
 
 export default App;
